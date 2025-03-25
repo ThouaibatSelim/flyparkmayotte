72e274e4485ce32c0529972ce47f2dfbd6778033
unknown <selimthouaibat9@gmail.com>
2025-03-25 13:50:06 +0300

    ajout de devis et modification du footer

--- flypark/src/components/App.js
+++ flypark/src/components/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 // import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import Accueil from './Accueil';
-=======
-<<<<<<< HEAD
+
 import Accueil from './Accueil';
 import Reservation from './Reservation';
 import Services from './Services';
@@ -14,18 +11,6 @@ import Inscription from './Inscription';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Devis from './Devis';
-=======
-import Accueil from './accueil';
->>>>>>> ca0e5f18cf23355e4bf3126349cbd9f3b016676f
-import Reservation from './reservation';
-import Services from './services';
-import APropos from './apropos';
-import Connexion from './connexion';
-import Inscription from './inscription';
-import Navbar from './Navbar';
-import Footer from './footer';
-// import Layout from './layout';
->>>>>>> ab4a53a0dec3dcc722e0862412fb152645963dac
 
 function App() {
   return (
